d246c996cf9f9746a17641c72db69496b805c109
Santiago Vargas N <sanvargas02@hotmail.com>
2023-07-12 08:14:41 -0500

    Fotografía mitad Módulo 5 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HerosModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HerosModule
+    HerosModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
